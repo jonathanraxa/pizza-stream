c4344eb373d802e2028604a084f25ee62347422f
Jonathan Raxa <jonathanraxa@gmail.com>
2025-09-03 21:19:08 -0500

    change start to button; changed loading state; update vars; clean up

--- js/test.js
+++ js/test.js
@@ -1,79 +1,68 @@
-/* 
-    quick script used to test 
-    overall functionality of the webpage
-*/
-window.onload = () => {
+const initOrder = () => {
+    const orderSteps = document.querySelectorAll('.order--step');
+    const imgContainers = document.querySelectorAll('.panel');
+    const loadingState = document.getElementById('loading');
+    const getTime = document.getElementById('remaining-time');
+    const leadText = document.getElementById('lead-text');
 
-    (function() {
-      setTimeout(()=>{
-          if (confirm("Run prototype?")) {
-              initPrototype();
-          }
-      }, 4000);
-    })();
-  
-
-    let initPrototype = () => {
-
-        let orderSteps = document.querySelectorAll('.order--step');
-        let imgContainers = document.querySelectorAll('.panel');
-        let loadingImg = document.getElementById('loading-img');
-        let getTime = document.getElementById('remaining-time');
-        let leadText = document.getElementById('lead-text');
-
-        // loop through images and change classes
-        let i = 0;
-        let interval = setInterval(function () {
-            if (i <= orderSteps.length + 1) {
-                if (i < 3) {
-                    changeToActive(i);
-                } else if (i === 3) {
-                    changeRemainingTime(3);
-                }
-                i++;
+    // loop through images and change classes
+    let i = 0;
+    const interval = setInterval(function () {
+        if (i <= orderSteps.length + 1) {
+            if (i < 3) {
+                changeToActive(i);
+            } else if (i === 3) {
+                changeRemainingTime(3);
             }
-            else {
-                clearInterval(interval);
-            }
-        }, 3000);
-
-        let changeToActive = currElement => {
-
-            if (currElement != 0) {
-                imgContainers[currElement].classList.remove("un_processed");
-                imgContainers[currElement].classList.add("active");
-                changeRemainingTime(currElement);
-            }
-            setTimeout(() => {
-                changeToProcessed(currElement);
-            }, 2000);
+            i++;
         }
-
-        let changeToProcessed = currElement => {
-            imgContainers[currElement].classList.add("processed");
+        else {
+            clearInterval(interval);
         }
+    }, 3000);
 
-        let changeRemainingTime = currTime => {
-            switch (currTime) {
-                case 0:
-                    getTime.innerHTML = "60 minutes";
-                    break;
-                case 1:
-                    getTime.innerHTML = "30 minutes";
-                    break;
-                case 2:
-                    getTime.innerHTML = "10 minutes";
-                    break;
-                case 3:
-                    leadText.remove();
-                    loadingImg.remove();
-                    getTime.innerHTML = "Delivered!";
-                    break;
-                default:
-                    getTime.innerHTML = ' -- minutes'
-            }
+    const changeToActive = currElement => {
+        if (currElement != 0) {
+            imgContainers[currElement].classList.remove("un_processed");
+            imgContainers[currElement].classList.add("active");
+            changeRemainingTime(currElement);
+        }
+        setTimeout(() => {
+            changeToProcessed(currElement);
+        }, 2000);
+    };
+
+    const changeToProcessed = currElement => {
+        imgContainers[currElement].classList.add("processed");
+    };
+
+    const changeRemainingTime = currTime => {
+        switch (currTime) {
+            case 0:
+                getTime.innerHTML = "60 minutes";
+                break;
+            case 1:
+                getTime.innerHTML = "30 minutes";
+                break;
+            case 2:
+                getTime.innerHTML = "10 minutes";
+                break;
+            case 3:
+                leadText.remove();
+                loadingState.remove();
+                getTime.innerHTML = "Delivered!";
+                break;
+            default:
+                getTime.innerHTML = ' -- minutes'
         }
     }
 }
 
-    
+
+
+document.querySelector('.ps_button').addEventListener('click', () => {
+    initOrder();
+    document.querySelector('.ps_button').classList.add('hide');
+    document.querySelector('.confirmation--cont').classList.remove('hide');
+    document.querySelector('.confirmation--cont').classList.add('show');
+});
